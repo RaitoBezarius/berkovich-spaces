cac17b078438949605d3f8f3167a29668bb45493
Raito Bezarius <masterancpp@gmail.com>
2021-02-01 16:50:21 +0100

    for_mathlib(exp_log): add neq_one => nonzero log

---
+++ src/for_mathlib/exp_log.lean
@@ -0,0 +1,17 @@
+import analysis.special_functions.exp_log
+
+lemma real.log_ne_zero_of_ne_one (x: ℝ) (hx_pos: 0 < x) (hx: x ≠ 1): real.log x ≠ 0 :=
+begin
+  by_cases (1 < x),
+  exact ne_of_gt (real.log_pos h),
+  push_neg at h,
+  exact ne_of_lt (real.log_neg hx_pos (lt_of_le_of_ne h hx)),
+end
+
+lemma real.ne_one_of_log_ne_zero (x: ℝ) (hx_pos: 0 < x) (hlog: real.log x ≠ 0): x ≠ 1 :=
+begin
+  by_cases (0 < real.log x),
+  sorry, -- we don't really care.
+end
+
+lemma real.log_ne_zero_iff (x: ℝ): (real.log x ≠ 0 ↔ x ≠ 1) := sorry
